6ad14fe6756a3a22fd2b48fe09ecfc40c038c817
Phanuwat <laernoyats@gmail.com>
2023-10-03 23:11:21 +0700

    start new project

---
+++ ft_printft.h
@@ -0,0 +1,14 @@
+#ifndef FT_PRINTFT
+# define FT_PRINTF
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+////////////////////
+# include <stdio.h>
+///////////////////
+
+
+
+#endif
